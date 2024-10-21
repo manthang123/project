5550df46524e6cce94617169e0da5e0cae4e8c4d
manthang123 <manthanghonge123@gmail.com>
2024-10-21 17:18:29 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
-import SingupForm from './components/SingupForm.jsx';
+import SignupForm from './components/SingupForm.jsx';
 import LoginForm from './components/LoginForm.jsx';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/sidebar.jsx';
+import Topbar from './components/topbar.jsx';
+import Portfolio from './components/Portfolio';
+import Purchases from './components/Purchases';
 
-function App() {
+const Layout = ({ children }) => {
+  const location = useLocation();
+  const isAuthRoute = ['/login', '/signup'].includes(location.pathname);
+
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/signup" element={<SingupForm />} />
-          <Route path="/login" element={<LoginForm />} />
-        </Routes>
+    <div className="flex h-screen bg-gray-300">
+      {!isAuthRoute && <Sidebar />}
+      <div className="flex-1 flex flex-col">
+        {!isAuthRoute && <Topbar />}
+        <main className="p-4 flex-1 overflow-y-auto">
+          {children}
+        </main>
       </div>
-    </Router>
+    </div>
   );
-}
+};
+
+const App = () => (
+  <Router>
+    <div className="bg-gray-200 min-h-screen">
+      <Routes>
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <div className="space-y-6">
+                <Portfolio />
+                <Purchases />
+              </div>
+            </Layout>
+          }
+        />
+      </Routes>
+    </div>
+  </Router>
+);
 
 export default App;
